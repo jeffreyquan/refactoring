8e65c8d4bacbb09e846123ae709f09ba0524e731
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-05-04 23:57:16 +1000

    changed function declaration to amountFor to remove play parameter

--- first_example/statement.js
+++ first_example/statement.js
@@ -1,7 +1,7 @@
 function statement(invoice, plays) {
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -20,7 +20,7 @@ function statement(invoice, plays) {
         break;
 
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
 
     return result;
@@ -40,7 +40,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
