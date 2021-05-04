5e286c127c6da590da11a3e88cfcdaafadf1d263
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-05-05 00:31:01 +1000

    shifted nested functions down

--- first_example/statement.js
+++ first_example/statement.js
@@ -1,4 +1,18 @@
 function statement(invoice, plays) {
+  let result = `Statement for ${invoice.customer}\n`;
+
+  for (let perf of invoice.performances) {
+    // print line for this order
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    } seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+
+  return result;
+
   function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
@@ -61,20 +75,6 @@ function statement(invoice, plays) {
     }
     return volumeCredits;
   }
-
-  let result = `Statement for ${invoice.customer}\n`;
-
-  for (let perf of invoice.performances) {
-    // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    } seats)\n`;
-  }
-
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-
-  return result;
 }
 
 module.exports = statement;
