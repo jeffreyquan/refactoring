d64bd817565dd8e11daa13de25212b453199f760
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-05-05 00:52:39 +1000

    moved volumeCreditsFor to statement to enrich data with volumeCredits for each performance

--- first_example/statement.js
+++ first_example/statement.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -39,6 +40,14 @@ function statement(invoice, plays) {
         throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
 
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 }
@@ -74,18 +83,10 @@ function renderPlainText(data, plays) {
     return result;
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
   function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += perf.volumeCredits;
     }
     return volumeCredits;
   }
