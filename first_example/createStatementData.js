3ad435169a75591b3d0315139eb58cac7a1b1271
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-05-05 22:20:04 +1000

    removed amountFor and volumeCreditsFor functions and call the new functions directly

--- first_example/createStatementData.js
+++ first_example/createStatementData.js
@@ -57,17 +57,12 @@ function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
-      .volumeCredits;
   }
 
   function totalAmount(data) {
