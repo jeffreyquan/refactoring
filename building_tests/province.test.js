58e0139ec21c7c96d6c0d2b9eb6726352c10552f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-05-13 21:34:21 +1000

    moved standard fixture in both tests into a beforeEach

--- building_tests/province.test.js
+++ building_tests/province.test.js
@@ -14,13 +14,15 @@ function sampleProvinceData() {
 }
 
 describe("province", function () {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
 
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
   });
 });
