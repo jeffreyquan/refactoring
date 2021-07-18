9291e7123d341a254ba45925e90b955657ad541c
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-07-18 14:10:05 +1000

    ✅ completed class example

--- a_first_set_of_refactorings/extract_variable.js
+++ a_first_set_of_refactorings/extract_variable.js
@@ -60,3 +60,36 @@ class Order {
     );
   }
 }
+
+// Extract variables in price
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get quantityDiscount() {
+    return (
+      this.basePrice - Math.max(0, this.quantity - 500) * this.itemPrice * 0.05
+    );
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+}
