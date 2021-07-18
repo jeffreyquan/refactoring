4c0bfa68f8c4f9549a078d04924cdfec58b3eaea
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-07-18 14:06:30 +1000

    added example using class

--- a_first_set_of_refactorings/extract_variable.js
+++ a_first_set_of_refactorings/extract_variable.js
@@ -1,3 +1,4 @@
+// Example 1
 function price(order) {
   //price is base price quantity discount + shipping;
   return (
@@ -35,4 +36,27 @@ function price(order) {
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   const shipping = Math.min(basePrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping;
+}
+
+// Example 2
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.quantity * this.itemPrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+    );
+  }
 }
