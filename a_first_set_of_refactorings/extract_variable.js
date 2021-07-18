70a3fa100c0b9f07ea0807560c62c72981048d11
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-07-18 13:59:57 +1000

    ✅ completed example on extract variable

--- a_first_set_of_refactorings/extract_variable.js
+++ a_first_set_of_refactorings/extract_variable.js
@@ -6,3 +6,33 @@ function price(order) {
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
 }
+
+// 1. Extract out order.quantiy * order.itemPrice
+function price(order) {
+  //price is base price quantity discount + shipping;
+  const basePrice = order.quantity * order.itemPrice;
+  return (
+    basePrice -
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(basePrice * 0.1, 100)
+  );
+}
+
+// 2. Extract quantity discount
+function price(order) {
+  //price is base price quantity discount + shipping;
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
+}
+
+// 3. Extract shipping
+function price(order) {
+  //price is base price quantity discount + shipping;
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
+}
