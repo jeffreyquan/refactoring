725843d00099b76ed5dd82dd593f4f011ea54715
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-07-18 14:39:16 +1000

    ✅ completed example on changing function declaration

--- a_first_set_of_refactorings/change_function_declaration.js
+++ a_first_set_of_refactorings/change_function_declaration.js
@@ -44,8 +44,49 @@ class Book {
 // replace callers to use zz_addReservation and then rename zz_addReservation to addReservation removing the original
 
 // Example 3
+// Aim is to make it more usable in more context by removing the dependency on the customer
 function inNewEngland(aCustomer) {
   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
 }
 
 const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
+
+// 1. Extract variable
+function inNewEngland(aCustomer) {
+  const stateCode = aCustomer.address.state;
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+// 2. Extract function
+function inNewEngland(aCustomer) {
+  const stateCode = aCustomer.address.state;
+  return xxNEWinNewEngland(stateCode);
+}
+
+function xxNEWinNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+// 3. Inline variable
+function inNewEngland(aCustomer) {
+  return xxNEWinNewEngland(aCustomer.address.state);
+}
+
+function xxNEWinNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+// 4. Inline function
+const newEnglanders = someCustomers.filter((c) =>
+  xxNEWinNewEngland(c.address.state)
+);
+
+// 5. Change function declaration to change name of new function to original
+
+function inNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+const newEnglanders = someCustomers.filter((c) =>
+  inNewEngland(c.address.state)
+);
