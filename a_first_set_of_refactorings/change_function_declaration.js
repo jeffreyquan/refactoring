01588fbbdb016b0d7446cf3417e84b4234d00e02
Jeffrey Quan <jeffrey.quan@hotmail.com>
2021-07-18 14:22:14 +1000

    ✅ completed example on changing function declaration

--- a_first_set_of_refactorings/change_function_declaration.js
+++ a_first_set_of_refactorings/change_function_declaration.js
@@ -1,3 +1,9 @@
 function circum(radius) {
+  // call new function declaration as I am migrating callers from using circum to circumference
+  return circumference(radius);
+}
+
+// change declaration to something more sensible
+function circumference(radius) {
   return 2 * Math.PI * radius;
 }
